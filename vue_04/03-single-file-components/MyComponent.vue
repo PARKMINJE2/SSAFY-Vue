5b0f4e2926e2a0680264e249ae71e8164a45303a
PARKMINJE2 <alswp936@naver.com>
2024-11-06 12:42:38 +0900

    2024-11-06

---
+++ vue_04/03-single-file-components/MyComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="greeting">
+    {{ msg }}
+  </div>
+</template>
+
+<!-- Vue CDN 불필요 -->
+<!-- ↓setup 함수 -->
+<script setup>
+// const { ref } = Vue
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+// return 작성 불필요
+</script>
+
+<style scoped>
+.greeting {
+  color: red;
+}
+</style>
